c2832c65b30aa40305a3aab5dea2196a7af8f2c5
Waqas Ali <waqas.abbasi@outlook.com>
2018-02-24 11:48:36 +0800

    Smart Contract v1

--- ethereum/contracts/CaseFactory.sol
+++ ethereum/contracts/CaseFactory.sol
@@ -1,90 +1,39 @@
 pragma solidity ^0.4.17;
 
-contract CampaignFactory{
-    address[] public deployedCampagins;
-    
-    function createCampaign(uint minimum) public{
-        address newCampaign = new Campaign(minimum, msg.sender);
-        deployedCampagins.push(newCampaign);
+contract CaseFactory {
+    struct Case {
+        string title;
+        string description;
+        address caseAddress;
+        int8 status;
     }
-    
-    function getDeployedCampaigns() public view returns (address[]){
-        return deployedCampagins;
+
+    Case[] public cases;
+
+    function addCase(string title, string description) public {
+        address newCaseAddress = new LawCase(title, description, msg.sender);
+        Case memory newCase = Case({
+            title: title,
+            description: description,
+            caseAddress: newCaseAddress,
+            status: -1
+        });
+        cases.push(newCase);
     }
-    
+
+    function getCases() public view returns (Case[]) {
+        return cases;
+    }
 }
 
+contract LawCase {
+    address public client;
+    string public title;
+    string public description;
 
-contract Campaign {
-    struct Request{
-        string description;
-        uint value;
-        address recipient;
-        bool complete;
-        uint approvalCount;
-        mapping(address => bool)approvals;
+    function LawCase(string caseTitle, string caseDescription, address creator) public {
+        title = caseTitle;
+        description = caseDescription;
+        client = creator;
     }
-    
-    Request[] public requests;
-    address public manager;
-    uint public minimumContribution;
-    mapping(address => bool) public approvers;
-    uint public approversCount;
-    
-    modifier restricted(){
-        require(msg.sender == manager);
-        _;
-    }
-    
-    function Campaign(uint minimum, address creator) public {
-        manager = creator;
-        minimumContribution = minimum;
-    }
-    
-    function contribute() public payable {
-        require(msg.value > minimumContribution);
-        
-        approvers[msg.sender] = true;
-        
-        approversCount++;
-    }
-    
-    function createRequest(string description, uint value, address recipient) 
-        public restricted {
-        
-        Request memory newRequest = Request({
-            description: description,
-            value: value,
-            recipient: recipient,
-            complete: false,
-            approvalCount: 0
-        });
-        
-        requests.push(newRequest);
-    }
-    
-    function approveRequest(uint index) public {
-        Request storage request = requests[index];
-        
-        require(approvers[msg.sender]);
-        require(!request.approvals[msg.sender]);
-        
-       request.approvals[msg.sender] = true;
-        request.approvalCount++;
-    }
-    
-    function finalizeRequest(uint index) public restricted {
-        Request storage request = requests[index];
-        
-        require(request.approvalCount > (approversCount / 2));
-        
-        require(!request.complete);
-        
-        request.recipient.transfer(request.value);
-        request.complete = true;
-        
-        
-    }
-    
-    
 }
