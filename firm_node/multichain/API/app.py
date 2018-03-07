1f5c950a23c0dfdcf41c0a7e33a9b14129796c8a
Waqas Ali <waqas.abbasi@outlook.com>
2018-03-08 04:37:49 +0800

    Python API

---
+++ firm_node/multichain/API/app.py
@@ -0,0 +1,52 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+from Savoir import Savoir
+import json
+import time
+import datetime
+
+app = Flask(__name__)
+
+def connect():
+    with open('credentials.json') as json_data:
+        credentials = json.load(json_data)
+        json_data.close()
+    rpcuser = credentials["rpcuser"]
+    rpcpasswd = credentials["rpcpasswd"]
+    rpchost = credentials["rpchost"]
+    rpcport = credentials["rpcport"]
+    chainname = credentials["chainname"]
+    return Savoir(rpcuser, rpcpasswd, rpchost, rpcport, chainname)
+
+multichain = connect()
+
+@app.route('/')
+def index():
+    return 'Egalex API'
+
+@app.route('/add_loan_applicant_data',methods=['POST'])
+def add_loan_applicat_data():
+    if request.method == 'POST':
+        data = request.get_json()
+        finaldata = json.dumps(data)
+        hexval = finaldata.encode('utf-8')
+        curid = datetime.datetime.now()
+        multichain.publish("strm1", str(curid), hexval.hex())
+        return 'Success!'
+
+@app.route('/get_all_applicant_data',methods=['GET'])
+def get_all_applicant_data():
+    if request.method == 'GET':
+        multichain.subscribe("strm1")
+        data = multichain.liststreamitems("strm1")
+        print(data)
+        response = []
+        for x in data:
+            obj = json.loads(bytearray.fromhex(x['data']).decode())
+            response.append(obj)
+
+        return jsonify(response)
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0', port=5000)
--- firm_node/multichain/setup_node.sh
+++ firm_node/multichain/setup_node.sh
@@ -4,11 +4,11 @@ sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 sudo dpkg-reconfigure locales
 git config credential.helper store
 echo '2. Cloning github...'
-git clone https://github.com/piy0999/CreditSense-Private.git
+git clone https://github.com/piy0999/Egalex
 echo '3. Installing python3 pip...'
 sudo apt install python3-pip
 echo '4. Installing packages...'
-sudo pip3 install -r ~/CreditSense-Private/bank_node/requirements.txt
+sudo pip3 install -r ~/Egalex/firm_node/multichain/requirements.txt
 echo '5. Installing multichain...'
 cd ~/tmp
 wget https://www.multichain.com/download/multichain-1.0.4.tar.gz
@@ -16,13 +16,9 @@ tar -xvzf multichain-1.0.4.tar.gz
 cd multichain-1.0.4
 sudo mv multichaind multichain-cli multichain-util /usr/local/bin
 cd ~
-echo '6. Creating multichain chain and stream...'
-multichain-util create chain1
-sed -i -e 's/anyone-can-connect = false/anyone-can-connect = true/g' ~/.multichain/chain1/params.dat
-sed -i -e 's/anyone-can-send = false/anyone-can-send = true/g' ~/.multichain/chain1/params.dat
-sed -i -e 's/anyone-can-receive = false/aanyone-can-receive = true/g' ~/.multichain/chain1/params.dat
+echo '6. Connecting to multichain chain...'
+multichaind chain1@$1
 multichaind chain1 -daemon
-multichain-cli chain1 create stream strm1 true
 echo '7. Opening ports...'
 sudo ufw allow 22
 sudo ufw allow 5000
@@ -30,7 +26,7 @@ sudo ufw enable
 echo '8. Setting up local credentials for multichain...'
 port=`grep default-rpc-port ~/.multichain/chain1/params.dat | grep -oP '[0-9]{4}'`
 password=`grep rpcpassword  ~/.multichain/chain1/multichain.conf | cut -d'=' -f2`
-cat >~/CreditSense-Private/bank_node/API/credentials.json <<EOF
+cat >~/Egalex/firm_node/multichain/API/credentials.json <<EOF
     {
       "rpcuser": "multichainrpc",
       "rpcpasswd": "$password",
@@ -40,5 +36,5 @@ cat >~/CreditSense-Private/bank_node/API/credentials.json <<EOF
     }
 EOF
 echo '9. Starting flask server...'
-cd ~/CreditSense-Private/bank_node/API
+cd ~/Egalex/firm_node/multichain/API
 python3 app.py
